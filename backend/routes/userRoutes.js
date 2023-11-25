563e3367bea594db04c00d6b6dd1604868c320ae
Osmani Leon <osmanileon92@gmail.com>
2023-11-25 14:36:29 -0600

    install nodemailer

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,52 @@
+// routes/userRoutes.js
+
+const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+const router = express.Router();
+
+// POST /register - User registration
+router.post('/register', async (req, res) => {
+    const { firstName, lastName, email, password } = req.body;
+
+    // Check if user already exists
+    if (User.findByEmail(email)) {
+        return res.status(400).send('User already exists');
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user and save
+    const newUser = new User(Date.now().toString(), firstName, lastName, email, hashedPassword);
+    User.save(newUser);
+
+    res.status(201).send('User registered');
+});
+
+module.exports = router;
+
+
+// POST /login - User login
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    // Find the user by email
+    const user = User.findByEmail(email);
+
+    // Check if the user exists
+    if (!user) {
+        return res.status(401).send('User not found');
+    }
+
+    // Verify the password
+    const passwordMatch = await bcrypt.compare(password, user.passwordHash);
+
+    if (!passwordMatch) {
+        return res.status(401).send('Incorrect password');
+    }
+
+    // You can generate and send a JWT token here for authentication
+
+    res.status(200).send('User logged in');
+});
--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,15 @@ const app = express();
 // Access Environment Variables
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json()); // Middleware to parse JSON bodies
+
 app.get('/', (req, res) => {
   res.send('AxleAxis Backend Running');
 });
+
+// Integrating User Routes
+const userRoutes = require('./routes/userRoutes');
+app.use('/api/users', userRoutes); // User routes
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
